6569240e3fce92d28a089bb23b7b91a8200396cf
20171105130 <S20171105130@admindeiMac-36.local>
2018-06-21 09:46:52 +0800

    text6.21
    hallo world

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  2018年06月21日09:15:16  First
+//
+//  Created by 杨扬 on 18/6/21.
+//  Copyright © 2018年 杨扬. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+}
